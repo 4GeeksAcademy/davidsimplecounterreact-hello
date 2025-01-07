d08d613d9897e2a7e62066818a6b74a96b7d4641
davidmata97-cell <daviddemata97@gmail.com>
2025-01-07 20:58:01 +0000

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Include bootstrap npm library into the bundle
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+import SimpleCounter from "./component/counter.jsx"
 
-//import your own components
-import Home from "./component/home.jsx";
+// Include your index.scss file into the bundle
+import '../styles/styles.css';
 
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
+let counter = 0;
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+setInterval(() => {
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+    
+    root.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />
+    );
+}, 1000);
